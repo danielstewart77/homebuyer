3efa36248d8b88b73c3f5a30fd1515c89003fdc2
Daniel Stewart <daniel.stewart77@gmail.com>
2020-11-05 16:07:01 -0600

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,43 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { CompareComponent } from './compare/compare.component';
+import { FaqComponent } from './faq/faq.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
+const routes: Routes = [
+  {
+    path: 'about-us',
+    component: AboutUsComponent,
+  },
+  {
+    path: 'compare',
+    component: CompareComponent,
+  },
+  {
+    path: 'faq',
+    component: FaqComponent,
+  },
+  {
+    path: 'how-it-works',
+    component: HowItWorksComponent,
+  },
+  {
+    path: 'reviews',
+    component: ReviewsComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: '**', 
+    redirectTo: 'home'
+  }
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CompareComponent } from './compare/compare.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { FaqComponent } from './faq/faq.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CompareComponent,
+    AboutUsComponent,
+    FaqComponent,
+    ReviewsComponent,
+    HowItWorksComponent,
+
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    MatButtonModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
